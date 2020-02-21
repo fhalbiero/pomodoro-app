c92225e9d8b6703162c54fbae33998368eeb8466
unknown <fhalbiero@gmail.com>
2020-02-21 02:42:12 -0300

    Updates

---
+++ precache-manifest.733f99acaf41773efa11e54eca55d764.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7f8a833536230cf110cb29005cfca4",
+    "url": "/pomodoro-app/index.html"
+  },
+  {
+    "revision": "8f5d3369b37f4176f21c",
+    "url": "/pomodoro-app/static/css/main.eb4a9d72.chunk.css"
+  },
+  {
+    "revision": "8b15fd03fd6112117596",
+    "url": "/pomodoro-app/static/js/2.2d239585.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pomodoro-app/static/js/2.2d239585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5d3369b37f4176f21c",
+    "url": "/pomodoro-app/static/js/main.3f6253e0.chunk.js"
+  },
+  {
+    "revision": "f70d595899356680f167",
+    "url": "/pomodoro-app/static/js/runtime-main.ca24fecb.js"
+  },
+  {
+    "revision": "d6040c63cafad92b0c2933569de365c0",
+    "url": "/pomodoro-app/static/media/finish.d6040c63.mp3"
+  },
+  {
+    "revision": "b76b0645d3ba50d44def821b619ab0b6",
+    "url": "/pomodoro-app/static/media/start.b76b0645.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52ad77cc14ce8a690b12fe767e39c388.js"
+  "/pomodoro-app/precache-manifest.733f99acaf41773efa11e54eca55d764.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomodoro-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
